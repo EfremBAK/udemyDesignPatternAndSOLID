4f2c0d0aec5df6533cfd59ce7a1a73e44b5027ea
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-06-30 17:25:09 -0500

    dependency inversion
    instead of directly writing a message in the main method, we have a MessagePrinter
    class that we instantiated and we will pass the JSONFormatter and print out msg

---
+++ dependency_inversion/src/main/java/Formatter.java
@@ -0,0 +1,7 @@
+
+
+public interface Formatter {
+	
+	public String format(Message message) throws FormatException;
+	
+}
---
+++ dependency_inversion/src/main/java/TextFormatter.java
@@ -0,0 +1,8 @@
+
+
+public class TextFormatter implements Formatter{
+
+	public String format(Message message){
+		return message.getTimestamp()+":"+message.getMsg();
+	}
+}
