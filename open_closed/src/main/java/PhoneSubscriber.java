bcb5171848ad75b21fc2cbcaa8b24997f875b2da
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-06-29 22:22:46 -0500

    Phone Service Application
    -move the duplicate fields and the methods to Subscriber class so that everything on the Subscriber class is closed to modification but only the abstract method that we have, i.e the calculate() method is open to modification. The calculate() is override in the classes that inherit the Subscribe class

---
+++ open_closed/src/main/java/PhoneSubscriber.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class PhoneSubscriber extends Subscriber {
+
+    @Override
+    public double calculateBill() {
+        List<CallHistory.Call> sessions = CallHistory.getCurrentCalls(subscriberId);
+        long totalDuration = sessions.stream().mapToLong(CallHistory.Call::getDuration).sum();
+        return totalDuration*baseRate/100;
+    }
+
+}
