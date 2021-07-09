dd117956d10758a7d4e38e15f16b3dd55dee5b37
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-07-09 14:14:34 -0500

    Composite Pattern
    file and directory heirarchy.

--- 06ProxyPattern/src/com/coffeepoweredcrew/proxy/dynamic/Client.java
+++
@@ -1,8 +0,0 @@
-package com.coffeepoweredcrew.proxy.dynamic;
-
-public class Client {
-
-	public static void main(String[] args) {
-		
-	}
-}
--- 06ProxyPattern/src/com/coffeepoweredcrew/proxy/dynamic/ImageInvocationHandler.java
+++
@@ -1,6 +0,0 @@
-package com.coffeepoweredcrew.proxy.dynamic;
-
-//Implement invocation handler. Your "proxy" code goes here.
-public class ImageInvocationHandler {
-	
-}
